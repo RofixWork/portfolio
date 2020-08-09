b86468a0eae6b15374a01e942e8413a3f36c5a3b
RofixWork <wourkout123@gmail.com>
2020-08-09 18:05:07 +0100

    Updates

---
+++ precache-manifest.f13911644103c1586d39aeead0177786.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2a167cce7cfa3c59af7e3b4a5c9386",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2a7627ee93f90cdc6a09",
+    "url": "/portfolio/static/css/main.576a9496.chunk.css"
+  },
+  {
+    "revision": "a9097ad9fb39b4e4d6bc",
+    "url": "/portfolio/static/js/2.a34032bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.a34032bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7627ee93f90cdc6a09",
+    "url": "/portfolio/static/js/main.96f853d1.chunk.js"
+  },
+  {
+    "revision": "ff9193a18fc3357125b0",
+    "url": "/portfolio/static/js/runtime-main.5c3fb0f9.js"
+  },
+  {
+    "revision": "332df03227e802d0a1cfcd3926d33f64",
+    "url": "/portfolio/static/media/bg-head.332df032.jpg"
+  }
+]);
